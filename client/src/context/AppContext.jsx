d4bceef08a10f791459f3e62ba88a85acbd43fad
Shilpa1210-eng <shilpa.nair.1210@gmail.com>
2025-08-04 15:42:45 +0530

    Explore Module UI complete

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,7 +1,8 @@
 import {createContext, useEffect, useState} from "react";
 import {fetchCategories} from "../Service/CategoryService.js";
-//import {fetchItems} from "../Service/ItemService.js";
+import {fetchItems} from "../Service/ItemService.js";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext(null);
 
 export const AppContextProvider = (props) => {
@@ -9,9 +10,9 @@ export const AppContextProvider = (props) => {
     const [categories, setCategories] = useState([]);
     const [itemsData, setItemsData] = useState([]);
     const [auth, setAuth] = useState({token: null, role: null});
-    //const [cartItems, setCartItems] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
 
-   /* const addToCart = (item) => {
+    const addToCart = (item) => {
         const existingItem = cartItems.find(cartItem => cartItem.name === item.name);
         if (existingItem) {
             setCartItems(cartItems.map(cartItem => cartItem.name === item.name ? {...cartItem, quantity: cartItem.quantity + 1} : cartItem));
@@ -19,7 +20,7 @@ export const AppContextProvider = (props) => {
             setCartItems([...cartItems, {...item, quantity: 1}]);
         }
     }
-    
+
     const removeFromCart = (itemId) => {
         setCartItems(cartItems.filter(item => item.itemId !== itemId));
     }
@@ -27,7 +28,7 @@ export const AppContextProvider = (props) => {
     const updateQuantity = (itemId, newQuantity) => {
         setCartItems(cartItems.map(item => item.itemId === itemId ? {...item, quantity: newQuantity} : item));
     }
-    */
+
     useEffect(() => {
         async function loadData() {
             if (localStorage.getItem("token") && localStorage.getItem("role")) {
@@ -37,10 +38,10 @@ export const AppContextProvider = (props) => {
                 );
             }
             const response = await fetchCategories();
-           // const itemResponse = await fetchItems();
-           // console.log('item response', itemResponse);
+            const itemResponse = await fetchItems();
+            console.log('item response', itemResponse);
             setCategories(response.data);
-            //setItemsData(itemResponse.data);
+            setItemsData(itemResponse.data);
 
         }
         loadData();
@@ -49,11 +50,11 @@ export const AppContextProvider = (props) => {
     const setAuthData = (token, role) => {
         setAuth({token, role});
     }
-    /*
+
     const clearCart = () => {
         setCartItems([]);
     }
-    */
+
     const contextValue = {
         categories,
         setCategories,
@@ -61,11 +62,11 @@ export const AppContextProvider = (props) => {
         setAuthData,
         itemsData,
         setItemsData,
-        // addToCart,
-        // cartItems,
-        // removeFromCart,
-        // updateQuantity,
-        // clearCart
+        addToCart,
+        cartItems,
+        removeFromCart,
+        updateQuantity,
+        clearCart
     }
 
     return <AppContext.Provider value={contextValue}>
