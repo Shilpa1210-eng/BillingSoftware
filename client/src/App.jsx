08d9d140fe2ec9395b389b8fcc2ef4823075060f
shanthi1710 <shanthinathshedabale@gmail.com>
2025-08-04 12:11:38 +0530

    implement login UI components and functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import Menubar from './components/Menubar/Menubar.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import Explore from './pages/Explore/Explore.jsx';
 import ManageItems from './pages/ManageItems/ManageItems.jsx';
 import ManageCategory from './pages/ManageCategory/ManageCategory.jsx';
 import ManageUsers from './pages/ManageUsers/ManageUsers.jsx';
 import { Toaster } from 'react-hot-toast';
+import Login from './pages/Login/Login.jsx';
 
 const App = () => {
+  const location = useLocation(); 
   return (
-    <>
+    <div>
       <div>
-        <Menubar />
+        {location.pathname !=="/login" && <Menubar />}
         <Toaster />
           <Routes>
             <Route path="/" element={<Dashboard />} />
@@ -20,9 +22,10 @@ const App = () => {
             <Route path="/items" element={<ManageItems />} />
             <Route path="/category" element={<ManageCategory />} />
             <Route path="/users" element={<ManageUsers />} />
+             <Route path="/login" element={<Login/>}/>
           </Routes>
         </div>
-    </>
+    </div>
   )
 }
 
