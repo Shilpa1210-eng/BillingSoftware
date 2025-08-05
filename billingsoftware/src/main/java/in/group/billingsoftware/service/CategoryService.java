3be7839090c70b0e300eabfbe90dbdc1765aac3d
suri193 <kr.surndra101@gmail.com>
2025-08-06 00:30:11 +0530

    Feature: Image storing

--- billingsoftware/src/main/java/in/group/billingsoftware/service/CategoryService.java
+++ billingsoftware/src/main/java/in/group/billingsoftware/service/CategoryService.java
@@ -4,11 +4,12 @@ import in.group.billingsoftware.io.CategoryRequest;
 import in.group.billingsoftware.io.CategoryResponse;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface CategoryService {
 
-    CategoryResponse add(CategoryRequest request, MultipartFile file);
+    CategoryResponse add(CategoryRequest request, MultipartFile file) throws IOException;
 
     List<CategoryResponse> read();
 
